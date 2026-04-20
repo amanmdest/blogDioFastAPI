2ef28bb43f8868121d8373321b96891f89db044c
amanmdest <amanmdest@gmail.com>
2026-04-20 16:41:40 -0300

    async sqlalchemy connection with databases[aiosqlite]

--- controllers/post.py
+++ controllers/post.py
@@ -5,10 +5,56 @@ from typing import Annotated
 from fastapi import APIRouter, Cookie, Header, Response, status
 from schemas.post import PostIn
 from views.post import PostOut
-from models.FakeDb import fake_db
 
 dt = datetime.datetime
 router = APIRouter(prefix='/posts')
+fake_db = [
+        {
+        'title': f'Criando uma aplicação com Django', 
+        'date': dt.now(datetime.timezone.utc),
+        'published': True,
+    },
+    {
+        'title': f'Criando uma aplicação com FastAPI', 
+        'date': dt.now(datetime.timezone.utc),
+        'published': True,
+    },
+    {
+        'title': f'Criando uma aplicação com Flask', 
+        'date': dt.now(datetime.timezone.utc),
+        'published': True,
+    },
+    {
+        'title': f'Criando uma aplicação com Falcon', 
+        'date': dt.now(datetime.timezone.utc),
+        'published': True,
+    },
+    {
+        'title': f'Criando uma aplicação com Tornado', 
+        'date': dt.now(datetime.timezone.utc),
+        'published': True,
+    },
+    {
+        'title': f'Criando uma aplicação com Pyramid', 
+        'date': dt.now(datetime.timezone.utc),
+        'published': True,
+    },
+    {
+        'title': f'Criando uma aplicação com CherryPy', 
+        'date': dt.now(datetime.timezone.utc),
+        'published': True,
+    },
+    {
+        'title': f'Criando uma aplicação com Cottle', 
+        'date': dt.now(datetime.timezone.utc),
+        'published': True,
+    },
+    {
+        'title': f'Criando uma aplicação com Aiohttp', 
+        'date': dt.now(datetime.timezone.utc),
+        'published': True,
+    },
+]
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=PostOut)
