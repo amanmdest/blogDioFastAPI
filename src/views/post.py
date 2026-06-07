12ae5aba935d8de3cc3dc8f5fa6d90f34674d5d5
amanmdest <amanmdest@gmail.com>
2026-06-06 23:00:47 -0300

    NaiveDatetime

--- src/views/post.py
+++ src/views/post.py
@@ -1,16 +1,16 @@
-from pydantic import AwareDatetime, BaseModel
+from pydantic import AwareDatetime, BaseModel, NaiveDatetime
 
 
 class PostOut(BaseModel):
     id: int
     title: str
     content: str
-    updated_at: AwareDatetime | None
-    published_at: AwareDatetime | None
+    updated_at: AwareDatetime | NaiveDatetime | None
+    published_at: AwareDatetime | NaiveDatetime | None
 
 
 class PostUpdated(BaseModel):
     id: int
     title: str
     content: str
-    updated_at: AwareDatetime | None
+    updated_at: AwareDatetime | NaiveDatetime | None
